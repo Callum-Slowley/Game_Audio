a271bcbb59471d408883cc2a1f76c3885526075e
cal <callum_slowley@hotmail.com>
2022-05-24 14:22:19 +0100

    trying to get libpb to work

--- GamesAudioGame/Assets/Scripts/DestructableObjects.cs
+++ GamesAudioGame/Assets/Scripts/DestructableObjects.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class DestructableObjects : MonoBehaviour
 {
+    public LibPdInstance pdPatch;
     public bool isOnFire = false;
     public GameObject[] fires;
+    public float fireTimer =0;
+    public float fireTimerMax=6;
 
     // Update is called once per frame
     void Update()
     {
         if (isOnFire)
         {
+            fireTimer+= Time.deltaTime;
+
             foreach (GameObject fire in fires)
             {
                 fire.SetActive(true);
             }
+            //pdPatch.SendFloat("fireStart",0.0f);
+        }
+
+        if(fireTimer >= fireTimerMax){
+            isOnFire=false;
+            Destroy(this.gameObject);
         }
     }
 }
